f8063bea2216ea928e8a58909c9f15b6957ad2d9
李琦 <liqi@abrain.com.cn>
2018-09-07 18:56:50 +0800

    test1

--- src/main/java/com/abrainyun/ldc/midware/eureka/LdcTestDeployApplication.java
+++ src/main/java/com/abrainyun/ldc/midware/eureka/LdcTestDeployApplication.java
@@ -2,8 +2,10 @@ package com.abrainyun.ldc.midware.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class LdcTestDeployApplication {
 
 	public static void main(String[] args) {
